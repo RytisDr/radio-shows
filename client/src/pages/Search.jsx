313358e0fcd5a4266fde9cd58c1c49c1bdb1deaf
RytisDr <r.drazdauskas@gmail.com>
2020-06-05 19:22:16 +0200

    Now fetches recent radio shows on homepage.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,24 @@
-import React from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
 const Search = () => {
-  return <h1>Search for an NTS Show.</h1>;
+  const pageLimit = 10;
+  const endP = `https://api.mixcloud.com/NTSRadio/cloudcasts/?limit=${pageLimit}`;
+  const [results, setResults] = useState(null);
+  const [initialFetch, setInitialFetch] = useState(true);
+  const fetchShows = useCallback(() => {
+    fetch(endP)
+      .then((e) => e.json())
+      .then((res) => setResults(res.data));
+  }, [endP]);
+  useEffect(() => {
+    fetchShows();
+  }, [fetchShows]);
+  return (
+    <>
+      <h1>Search for an NTS Show.</h1>
+      {initialFetch && <h1>Or Browse Recently Uploaded Shows:</h1>}
+      {results &&
+        results.map((result) => <h2 key={result.key}>{result.name}</h2>)}
+    </>
+  );
 };
 export default Search;
